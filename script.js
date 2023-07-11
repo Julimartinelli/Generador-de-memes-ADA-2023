76eee7432ba53b274cb07a4983c63bc553aa5c8f
Julimartinelli <julietaamartinelli@gmail.com>
2023-07-11 15:51:58 +0200

    Js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//IMAGEN
+const urlInput = document.getElementById('imagen-meme');
+console.log(urlInput)
+const imagenMeme = document.getElementById('imagen-meme');
+
+imagenMeme.addEventListener('input', (e)=> changeBackground(e))
+
+const changeBackground = (e) =>{
+    console.log(e.target.value)
+    imagenMeme.style.backgroundImage = `url ('${e.target.value}')`
+    imagenMeme.style.backgroundSize = 'cover';
+    imagenMeme.style.backgroundRepeat = 'no-repeat'
+    imagenMeme.style.backgroundPosition = 'center'
+}
