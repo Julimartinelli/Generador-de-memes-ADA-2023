bd1cc47d00753c6d392c86c623b1e0629a4f1e20
Julimartinelli <julietaamartinelli@gmail.com>
2023-07-16 13:55:19 +0200

    filtros

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert('hola')
 
 
 
@@ -9,7 +8,22 @@ alert('hola')
 
 
 
+//FILTROS
+const barraBrillo = document.getElementById('barra-brillo');
+const barraOpacidad = document.getElementById('barra-opacidad');
+const barraContraste = document.getElementById('barra-contraste')
+const barraDesenfoque = document.getElementById('barra-desenfoque')
+const barraEscalaGrises = document.getElementById('barra-escala-grises');
+const barraSepia = document.getElementById('barra-sepia');
+const barraHue = document.getElementById('barra-hue');
+const barraSaturado = document.getElementById('barra-saturado')
+const barraNegativo = document.getElementById('barra-negativo')
 
+const filtros = (e) => { 
+    imagenMeme.style.filter = `brightness(${barraBrillo.value}) opacity(${barraOpacidad.value}) blur(${barraDesenfoque.value}px) contrast(${barraContraste.value}%) grayscale(${barraEscalaGrises.value}%) hue-rotate(${barraHue.value}deg) sepia(${barraSepia.value}%) saturate(${barraSaturado.value}%) invert(${barraNegativo.value})`}
+
+
+barraBrillo.addEventListener('input', (e)=> filtros(e))
 
 
 
@@ -24,16 +38,14 @@ alert('hola')
 
 //IMAGEN
 const urlInput = document.getElementById('url-input');
-console.log(urlInput)
 const imagenMeme = document.getElementById('imagen-meme');
 
 urlInput.addEventListener('input', (e)=> changeBackground(e))
 
 const changeBackground = (e) =>{
+    console.log(e)
     imagenMeme.style.backgroundImage = `url ('${e.target.value}')`
     imagenMeme.style.backgroundSize = 'cover';
     imagenMeme.style.backgroundRepeat = 'no-repeat'
     imagenMeme.style.backgroundPosition = 'center'
 }
-
-
